51acbf9824ddd122246d1f0c6acac34c8514a443
CoodingPenguin <unodostre@naver.com>
2020-04-26 21:53:25 +0900

    Add csv exporter

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper.so import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
 
@@ -25,6 +26,22 @@ def report():
     else:
         redirect("/")
     return render_template("report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv", attachment_filename="jobs.csv", as_attachment=True)
+    except:
+        return redirect("/")
 
 
 app.run()
